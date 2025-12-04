bb65ed0b78321dae18d58fe19d110ca450e5fe14
Sawyer Wishart <busterblock01@outlook.com>
2025-12-04 08:58:52 -0600

    idk

--- Assets/Scripts/LevelProgressTracker.cs
+++ Assets/Scripts/LevelProgressTracker.cs
@@ -47,13 +47,13 @@ public class LevelProgressTracker : MonoBehaviour
     }
 
     public LevelInfo[] levels = new LevelInfo[7] { //the array of levels. - Nova
-        new (25f, 20f, 15f, 2 ),
-        new (25f, 20f, 15f, 3 ),
-        new (25f, 20f, 15f, 4 ),
+        new (20f, 15f, 10f, 2 ),
+        new (8f, 4f, 2f, 3 ),
+        new (15f, 10f, 5f, 4 ),
         new (25f, 20f, 15f, 5 ),
-        new (25f, 20f, 15f, 6 ),
+        new (6f, 5f, 4f, 6 ),
         new (25f, 20f, 15f, 7 ),
-        new (25f, 20f, 15f, 8 ),
+        new (21f, 14f, 7f, 8 ),
 
     };
 
