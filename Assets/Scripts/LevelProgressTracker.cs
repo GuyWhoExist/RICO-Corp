da04ff1b517ffea70ae226b044a5f21117c9bb44
Nova <nicholasacosta007@gmail.com>
2026-03-12 10:35:23 -0500

    Tutorial Progress

--- Assets/Scripts/LevelProgressTracker.cs
+++ Assets/Scripts/LevelProgressTracker.cs
@@ -189,7 +189,7 @@ public class LevelProgressTracker : MonoBehaviour
         new (25f, 20f, 15f, 7 ),
         new (21f, 14f, 7f, 8 ),
         new (200f, 115f, 45f, 9),
-        new (20f, 15f, 10f, 10),//who set this? 75 is not a valid time lol. 75 would be 0:75.00, which is not possible. - Nova
+        new (20f, 15f, 10f, 10),
        // new (120f, 75f, 45f, 10), //Level 1 v2 - Blockout Colby, Archetect tbd
        //new (200f, 115f, 45f, 9),
     };
