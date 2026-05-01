a5e150ad51214b01d094ab771b162c33f96b1a4e
Sawyer Wishart <busterblock01@outlook.com>
2026-05-01 09:51:26 -0500

    replaced all objects relabeled to hazard with their prefab variants, fixed timed death floors

--- Assets/Scripts/Environment Scripts/Sludge.cs
+++ Assets/Scripts/Environment Scripts/Sludge.cs
@@ -5,16 +5,21 @@ public class Sludge : MonoBehaviour
     //coded by sawyer
     private void OnCollisionEnter(Collision collision)
     {
+        Debug.Log("collided with");
        if (collision.transform.GetComponent<PlayerMovementTutorial>() != null)
         {
             collision.transform.GetComponent<PlayerMovementTutorial>().moveSpeed /= 2;
+            Debug.Log(collision.transform.GetComponent<PlayerMovementTutorial>().moveSpeed);
+
         }
     }
     private void OnCollisionExit(Collision collision)
     {
-        if(collision.transform.GetComponent<PlayerMovementTutorial>() != null)
+        Debug.Log("uncollided with");
+        if (collision.transform.GetComponent<PlayerMovementTutorial>() != null)
         {
             collision.transform.GetComponent<PlayerMovementTutorial>().moveSpeed *= 2;
+            Debug.Log(collision.transform.GetComponent<PlayerMovementTutorial>().moveSpeed);
         }
     }
 }
