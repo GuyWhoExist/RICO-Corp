bec4997f8dcdaca103a33d66464f3fbcc9518ad8
Sawyer Wishart <busterblock01@outlook.com>
2026-06-02 10:12:20 -0500

    fixed the player being able to buffer jump on kill planes

--- Assets/Scripts/Player Scripts/PlayerMovementTutorial.cs
+++ Assets/Scripts/Player Scripts/PlayerMovementTutorial.cs
@@ -103,11 +103,11 @@ public class PlayerMovementTutorial : MonoBehaviour
         hasPlayerShot = gameObject.transform.GetComponent<Shooting>();
         InvokeRepeating(nameof(PositionCheck), 0.2f, 0.2f);
         initialPosition = gameObject.transform.position;
-        Invoke(nameof(startChecker), 0.01f);
+        Invoke(nameof(StartChecker), 0.01f);
     }
 
 
-    private void startChecker()
+    private void StartChecker()
     {
         //Debug.Log("checking if player has done anything");
         if (gameObject.transform.position.x <= initialPosition.x+0.1 && gameObject.transform.position.x >= initialPosition.x - 0.1 && gameObject.transform.position.z <= initialPosition.z + 0.1 && gameObject.transform.position.z >= initialPosition.z - 0.1 && hasPlayerShot.shotDelay < 0)
@@ -116,7 +116,7 @@ public class PlayerMovementTutorial : MonoBehaviour
          * this comes with the side effect of the player being able to jump without starting the timer, but this rarely actually matters - Nova
         */
         {
-            Invoke(nameof(startChecker), 0.01f);
+            Invoke(nameof(StartChecker), 0.01f);
             //Debug.Log("player has not moved yet, looping...");
         }
         else
